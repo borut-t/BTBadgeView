9a6fbb819fb0bf8c262f2c110e5f865b6dca765c
Borut Tomažin <borut.tomazin@icloud.com>
2014-01-22 08:17:48 +0100

    Fixed issue #2

--- BTBadgeView/BTBadgeView.h
+++ BTBadgeView/BTBadgeView.h
@@ -1,7 +1,7 @@
 //
 //  BTBadgeView.h
 //
-//  Version 1.1
+//  Version 1.2
 //
 //  Created by Borut Tomazin on 12/04/2013.
 //  Copyright 2013 Borut Tomazin
@@ -32,44 +32,61 @@
 
 #import <UIKit/UIKit.h>
 
-#ifdef __IPHONE_6_0 // iOS6 and later
-#  define UITextAlignment                   NSTextAlignment
-#  define UITextAlignmentCenter             NSTextAlignmentCenter
-#  define UITextAlignmentLeft               NSTextAlignmentLeft
-#  define UITextAlignmentRight              NSTextAlignmentRight
-#  define UILineBreakModeTailTruncation     NSLineBreakByTruncatingTail
-#  define UILineBreakModeMiddleTruncation   NSLineBreakByTruncatingMiddle
-#endif
-
 @interface BTBadgeView : UIView
 
-// The current value displayed in the badge. Updating the value will update the view's display
-@property (nonatomic,strong) NSString *value;
-// Indicates whether the badge view draws a dhadow or not.
+/**
+ The current value displayed in the badge. Updating the value will update the view's display.
+ */
+@property (nonatomic,copy) NSString *value;
+
+/**
+ Indicates whether the badge view draws a shadow or not.
+ */
 @property (nonatomic,assign) BOOL shadow;
-// The offset for the shadow, if there is one.
+
+/**
+ (Optional) The offset for the shadow.
+ */
 @property (nonatomic,assign) CGSize shadowOffset;
-// The base color for the shadow, if there is one.
-@property (nonatomic,strong) UIColor * shadowColor;
-// Indicates whether the badge view should be drawn with a shine
+
+/**
+ (Optional) The base color for the shadow.
+ */
+@property (nonatomic,strong) UIColor *shadowColor;
+
+/**
+ Indicates whether the badge view should be drawn with a shine.
+ */
 @property (nonatomic,assign) BOOL shine;
-// The font to be used for drawing the numbers. NOTE: not all fonts are created equal for this purpose. (Only "system fonts" should be used.)
+
+/**
+ The font to be used for drawing the numbers. NOTE: Only "system fonts" should be used.
+ */
 @property (nonatomic,strong) UIFont *font;
-// The color used for the background of the badge.
+
+/**
+ The color used for the background of the badge.
+ */
 @property (nonatomic,strong) UIColor *fillColor;
-// The color to be used for drawing the stroke around the badge.
+
+/**
+ The color to be used for drawing the stroke around the badge.
+ */
 @property (nonatomic,strong) UIColor *strokeColor;
-// The width for the stroke around the badge.
+
+/**
+ The width for the stroke around the badge.
+ */
 @property (nonatomic,assign) CGFloat strokeWidth;
-// The color to be used for drawing the badge's numbers.
+
+/**
+ The color to be used for drawing the badge's numbers.
+ */
 @property (nonatomic,strong) UIColor *textColor;
-// How the badge image hould be aligned horizontally in the view. 
-@property (nonatomic,assign) UITextAlignment alignment;
-// Returns the visual size of the badge for the current value. Not the same hing as the size of the view's bounds. (The badge view bounds should be wider than space needed to draw the badge.)
-@property (nonatomic,readonly) CGSize badgeSize;
-// The number of pixels between the number inside the badge and the stroke around the badge. This value is approximate, as the font geometry might effectively slightly increase or decrease the apparent pad.
-@property (nonatomic,assign) NSUInteger pad;
-// If YES, the badge will be hidden when the value is 0
-@property (nonatomic,assign) BOOL hideWhenZero;
+
+/**
+ If YES, the badge will be hidden when the value is 0 or empty string.
+ */
+@property (nonatomic,assign) BOOL hideWhenEmpty;
 
 @end
--- BTBadgeView/BTBadgeView.m
+++ BTBadgeView/BTBadgeView.m
@@ -1,7 +1,7 @@
 //
 //  BTBadgeView.m
 //
-//  Version 1.1
+//  Version 1.2
 //
 //  Created by Borut Tomazin on 12/04/2013.
 //  Copyright 2013 Borut Tomazin
@@ -31,17 +31,14 @@
 //
 
 #import "BTBadgeView.h"
-
-@interface BTBadgeView ()
-
-@end
+#import "NSString+BTUtils.h"
 
 @implementation BTBadgeView
 
 - (id)initWithFrame:(CGRect)frame 
 {
     if ((self = [super initWithFrame:frame])) {
-		[self initState];
+		[self initializate];
     }
     return self;
 }
@@ -49,7 +46,7 @@
 - (id)initWithCoder:(NSCoder *)decoder
 {
 	if ((self = [super initWithCoder:decoder])) {
-		[self initState];
+		[self initializate];
     }
     return self;
 }
@@ -57,21 +54,19 @@
 
 
 #pragma mark Private methods
-- (void)initState
+- (void)initializate
 {	
 	self.opaque = NO;
-	self.pad = 2;
 	self.font = [UIFont boldSystemFontOfSize:16];
 	self.shadow = YES;
 	self.shadowOffset = CGSizeMake(0, 3);
 	self.shadowColor = [[UIColor blackColor] colorWithAlphaComponent:0.5];
 	self.shine = YES;
-	self.alignment = UITextAlignmentCenter;
 	self.fillColor = [UIColor redColor];
 	self.strokeColor = [UIColor whiteColor];
 	self.strokeWidth = 2.0;
 	self.textColor = [UIColor whiteColor];
-    self.hideWhenZero = NO;
+    self.hideWhenEmpty = NO;
 	
 	self.backgroundColor = [UIColor clearColor];
 }
@@ -80,7 +75,7 @@
 {
 	CGRect viewBounds = self.bounds;
 	CGContextRef context = UIGraphicsGetCurrentContext();
-	CGSize numberSize = [_value sizeWithFont:self.font];
+    CGSize numberSize = [_value textSizeWithFont:self.font fieldSize:CGSizeMake(self.bounds.size.width, self.bounds.size.height)];
 	CGPathRef badgePath = [self newBadgePathForTextSize:numberSize];
 	CGRect badgeRect = CGPathGetBoundingBox(badgePath);
 	
@@ -98,23 +93,8 @@
 	badgeRect.size.width += ceilf(self.strokeWidth / 2);
 	badgeRect.size.height += ceilf(self.strokeWidth / 2);
 	
-	CGPoint ctm;
-	
-	switch (self.alignment) {
-		case NSTextAlignmentJustified:
-		case NSTextAlignmentNatural:
-		case UITextAlignmentCenter:
-			ctm = CGPointMake(round((viewBounds.size.width - badgeRect.size.width)/2),
+	CGPoint ctm = CGPointMake(round((viewBounds.size.width - badgeRect.size.width)/2),
                               round((viewBounds.size.height - badgeRect.size.height)/2));
-			break;
-		case UITextAlignmentLeft:
-			ctm = CGPointMake(0, round((viewBounds.size.height - badgeRect.size.height)/2));
-			break;
-		case UITextAlignmentRight:
-			ctm = CGPointMake((viewBounds.size.width - badgeRect.size.width),
-                              round((viewBounds.size.height - badgeRect.size.height)/2));
-			break;
-	}
 	
 	CGContextTranslateCTM(context, ctm.x, ctm.y);
 
@@ -184,9 +164,6 @@
 	CGContextSaveGState(context);
 	CGContextSetFillColorWithColor(context, self.textColor.CGColor);
     
-    NSLog(@"point: %@",NSStringFromCGPoint(ctm));
-//	CGPoint textPt = CGPointMake(ctm.x + (badgeRect.size.width - numberSize.width)/2 ,
-//                                 ctm.y + (badgeRect.size.height - numberSize.height)/2);
     CGPoint textPt = CGPointMake(rect.size.width/2.f - numberSize.width/2.f,
                                  rect.size.height/2.f - numberSize.height/2.f);
 	
@@ -198,7 +175,7 @@
 
 - (CGPathRef)newBadgePathForTextSize:(CGSize)inSize
 {
-	CGFloat arcRadius = ceil((inSize.height+self.pad)/2.0);
+	CGFloat arcRadius = ceil((inSize.height)/2.0);
 	
 	CGFloat badgeWidthAdjustment = inSize.width - inSize.height/2.0;
 	CGFloat badgeWidth = 2.0*arcRadius;
@@ -223,26 +200,11 @@
 #pragma mark Property methods
 - (void)setValue:(NSString *)inValue
 {
-    _value = inValue;
-    self.hidden = self.hideWhenZero && _value == 0;
+    _value = [inValue trim];
+    self.hidden = self.hideWhenEmpty && (_value.length == 0 ||
+                                         (_value.length == 1 &&[_value integerValue] == 0));
 	
 	[self setNeedsDisplay];
-}
-
-- (CGSize)badgeSize
-{
-	CGSize numberSize = [_value sizeWithFont:self.font];
-	CGPathRef badgePath = [self newBadgePathForTextSize:numberSize];
-	CGRect badgeRect = CGPathGetBoundingBox(badgePath);
-	
-	badgeRect.origin.x = 0;
-	badgeRect.origin.y = 0;
-	badgeRect.size.width = ceil(badgeRect.size.width);
-	badgeRect.size.height = ceil(badgeRect.size.height);
-	
-	CGPathRelease(badgePath);
-	
-	return badgeRect.size;
 }
 
 @end
